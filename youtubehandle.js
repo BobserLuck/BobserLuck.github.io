1cca7d434cc2ead5ddb68394ece316ff2df7e3da
BobserLuck <i.hollenberry@gmail.com>
2016-09-09 22:57:38 -0700

    Update youtubehandle.js

--- youtubehandle.js
+++ youtubehandle.js
@@ -1,1 +1,11 @@
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
 
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See https://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyCNjPfdAsqbSqcUKE4sXP_iIXvgV8Tndo0');
+
+    search();
+}
